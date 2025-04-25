49c4bcc3d28b641a7a06b8fb4594e37d75532b8e
YP Yoo <ypy1324@gmail.com>
2025-04-24 21:37:01 -0400

    menu page design in progress

--- client/src/pages/orderNow/Menu.jsx
+++ client/src/pages/orderNow/Menu.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import mainBurger from "../../assets/images/mainBurger.png";
 
 function Menu() {
   const params = useParams();
+  const items = [
+    { name: "Egg McMuffin", price: 6.99, rating: 4.9, ratingCount: 12 },
+    { name: "Cheeseburger Deluxe", price: 8.99, rating: 4.3, ratingCount: 42 },
+    { name: "Big Mac", price: 2.99, rating: 2.1, ratingCount: 104 },
+    { name: "McSpicy", price: 1.99, rating: 4.4, ratingCount: 3 },
+    { name: "Filet-O-Fish", price: 12.99, rating: 3.9, ratingCount: 72 },
+    {
+      name: "Chicken Breakfast Wrap",
+      price: 0.99,
+      rating: 1.9,
+      ratingCount: 401,
+    },
+  ];
 
-  return <div className="menu-wrapper">{params.category}</div>;
+  return (
+    <div className="menu-wrapper">
+      <div className="menu-header">{params.category}</div>
+      {items.map((item, i) => {
+        return (
+          <div key={i} className="menu-item">
+            <img className="item-image" src={mainBurger} />
+            <div className="item-name">{item.name}</div>
+            <div className="item-price">${item.price}</div>
+            <div className="item-rating">
+              {item.rating} ({item.ratingCount})
+            </div>
+            <div className="item-select">Select</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Menu;
