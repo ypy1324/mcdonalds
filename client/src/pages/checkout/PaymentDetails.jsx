409162d787bf20d686dc128fb2ebbec892102438
YP Yoo <ypy1324@gmail.com>
2025-05-03 23:11:57 -0400

    paymentdetails component fixed

--- client/src/pages/checkout/PaymentDetails.jsx
+++ client/src/pages/checkout/PaymentDetails.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Input from "../../common/input/Input";
 
 function PaymentDetails() {
-  return <div>PaymentDetails</div>;
+  return (
+    <div className="payment-details-wrapper">
+      <div>
+        <div className="payment-details-header">Credit Card Details</div>
+        <Input input="Name on card" type="text" />
+        <Input input="Card number" type="number" />
+        <Input input="MM / YY" type="text" />
+        <Input input="CVV" type="number" />
+      </div>
+      <div>
+        <div className="payment-details-header">Billing Address</div>
+        <Input input="Street" type="text" />
+        <Input input="City" type="text" />
+        <Input input="Province" type="text" />
+        <Input input="Zip code" type="text" />
+      </div>
+    </div>
+  );
 }
 
 export default PaymentDetails;
