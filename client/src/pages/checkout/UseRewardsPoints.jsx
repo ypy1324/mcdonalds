f151c225abcd2ecd5ed072559209f8aeefa8efb7
YP Yoo <ypy1324@gmail.com>
2025-05-05 22:26:11 -0400

    UseRewardPoints component design done

--- client/src/pages/checkout/UseRewardsPoints.jsx
+++ client/src/pages/checkout/UseRewardsPoints.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function UseRewardsPoints() {
-  return <div>UseRewardsPoints</div>;
+  return (
+    <div className="use-points-wrapper">
+      <div className="use-points-header">Use your rewards points</div>
+      <div className="use-points-subheader">Current rewards points:</div>
+      <div className="rewards-points">837 points</div>
+      <div className="checkbox-wrapper">
+        <input type="checkbox" />
+        <label>Apply</label>
+      </div>
+    </div>
+  );
 }
 
 export default UseRewardsPoints;
