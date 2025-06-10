f7c79410cd870f13a64a030548bbe5ca0234f733
YP Yoo <ypy1324@gmail.com>
2025-06-09 22:12:15 -0400

    show user info on header when signed in

--- client/src/common/header/Header.jsx
+++ client/src/common/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
+import { FaRegUser } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
 import { useSelector } from "react-redux";
 import firebase from "../../firebase";
 import "./Header.css";
@@ -44,7 +47,25 @@ function Header() {
           </Nav>
           <Nav>
             {user.accessToken ? (
-              <Navbar.Text onClick={() => logoutHandler()}>Logout</Navbar.Text>
+              <Dropdown>
+                <Dropdown.Toggle className="sign-in-user">
+                  Hello, Mark!
+                </Dropdown.Toggle>
+                <Dropdown.Menu className="dropdown-menu">
+                  <Dropdown.Item>
+                    <NavLink to="/profile" className="dropdown-items">
+                      <FaRegUser size="1.5rem" />
+                      <a className="dropdown-text">Profile</a>
+                    </NavLink>
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => logoutHandler()}>
+                    <NavLink className="dropdown-items">
+                      <MdLogout size="1.5rem" />
+                      <a className="dropdown-text">Logout</a>
+                    </NavLink>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             ) : (
               <NavLink
                 to="/signin"
