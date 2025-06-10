156a018a69953ae5ad1287c3c92cb4550caa5209
YP Yoo <ypy1324@gmail.com>
2025-06-09 22:22:27 -0400

    disable review textarea if logged out

--- client/src/pages/itemDetail/ReviewArea.jsx
+++ client/src/pages/itemDetail/ReviewArea.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { GoHeart } from "react-icons/go";
+import { useSelector } from "react-redux";
 
 function ReviewArea() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className="review-area-wrapper">
       <div className="review-area-header">
         <div>Reviews</div>
         <div>2</div>
       </div>
-      <div className="review-input">
-        <textarea placeholder="Add a review..." />
-        <div>
-          <button>Submit</button>
+      {user.accessToken ? (
+        <div className="review-input">
+          <textarea placeholder="Add a review..." />
+          <div>
+            <button>Submit</button>
+          </div>
         </div>
-      </div>
+      ) : null}
       <div className="review-info">
         <div className="reviewer-info">
           <div>Mark</div>
