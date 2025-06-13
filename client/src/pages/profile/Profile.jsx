b35b7b1e9a48264fb2676cc1179e7d81d3815329
YP Yoo <ypy1324@gmail.com>
2025-06-12 21:34:32 -0400

    show user info at profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import Button from "../../common/button/Button";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Profile.css";
 
 function Profile() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className="profile-wrapper">
       <div className="profile-header">My Profile</div>
       <div className="profile-form">
         <div className="profile-field">
           <div className="profile-key">Name</div>
-          <div className="profile-value">Mark</div>
+          <div className="profile-value">{user.displayName}</div>
         </div>
         <hr className="profile-divider" />
         <div className="profile-field">
           <div className="profile-key">Email</div>
-          <div className="profile-value">mark123@gmail.com</div>
+          <div className="profile-value">{user.email}</div>
         </div>
         <hr className="profile-divider" />
         <div className="profile-field">
           <div className="profile-key">Rewards Points</div>
-          <div className="profile-value">465 pts</div>
+          <div className="profile-value">{user.rewardPoints} pts</div>
         </div>
         <hr className="profile-divider" />
         <div className="profile-field">
           <div className="profile-key">Address</div>
-          <div className="profile-value">25 Holly St, Toronto, ON, M4S 0E3</div>
+          <div className="profile-value">
+            {user.address.street}, {user.address.city}, {user.address.province},{" "}
+            {user.address.postalCode}
+          </div>
         </div>
         <Link to="/profile/changeAddress">
           <Button button="Change Address" />
