e6cce7e1aeec7be8334eeaec83943cd5e938dbe0
YP Yoo <ypy1324@gmail.com>
2025-06-29 22:41:33 -0400

    deleted unnecessary api for cart

--- server/router/cart.js
+++ server/router/cart.js
@@ -2,19 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { Cart } = require("../model/Cart");
 
-// API to fetch cart quantity
-// router.post("/getCartQuantity", (req, res) => {
-//   Cart.findOne({ userUid: req.body.userId })
-//     .exec()
-//     .then((cart) => {
-//       res.status(200).json({ success: true, quantity: cart.quantity });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(400).json({ success: false });
-//     });
-// });
-
 // API to add an item to the cart
 router.post("/addToCart", (req, res) => {
   let itemExists = false;
