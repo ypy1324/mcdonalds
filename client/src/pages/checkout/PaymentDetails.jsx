bd2e8a7a43494df1606e08734ef74675926d2a81
YP Yoo <ypy1324@gmail.com>
2025-07-06 23:07:04 -0400

    paymentDetail component input fixed

--- client/src/pages/checkout/PaymentDetails.jsx
+++ client/src/pages/checkout/PaymentDetails.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../common/input/Input";
 
 function PaymentDetails() {
+  const [nameOnCard, setNameOnCard] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [province, setProvince] = useState("");
+  const [zipCode, setZipCode] = useState("");
+
   return (
     <div className="payment-details-wrapper">
       <div className="payment-details">
         <div className="payment-details-header">Credit Card Details</div>
-        <Input input="Name on card" type="text" />
-        <Input input="Card number" type="number" />
-        <Input input="MM / YY" type="text" />
-        <Input input="CVV" type="number" />
+        <Input
+          input="Name on card"
+          type="text"
+          setValue={setNameOnCard}
+          value={nameOnCard}
+        />
+        <Input
+          input="Card number"
+          type="number"
+          setValue={setCardNumber}
+          value={cardNumber}
+        />
+        <Input
+          input="MM / YY"
+          type="text"
+          setValue={setExpiryDate}
+          value={expiryDate}
+        />
+        <Input input="CVV" type="number" setValue={setCvv} value={cvv} />
       </div>
       <div className="payment-details">
         <div className="payment-details-header">Billing Address</div>
-        <Input input="Street" type="text" />
-        <Input input="City" type="text" />
-        <Input input="Province" type="text" />
-        <Input input="Zip code" type="text" />
+        <Input input="Street" type="text" setValue={setStreet} value={street} />
+        <Input input="City" type="text" setValue={setCity} value={city} />
+        <Input
+          input="Province"
+          type="text"
+          setValue={setProvince}
+          value={province}
+        />
+        <Input
+          input="Zip code"
+          type="text"
+          setValue={setZipCode}
+          value={zipCode}
+        />
       </div>
     </div>
   );
