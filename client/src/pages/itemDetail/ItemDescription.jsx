369d0f09df4b96a0c8b71dc5745e75a4fda59c8f
YP Yoo <ypy1324@gmail.com>
2025-07-17 22:36:08 -0400

    minor fix on design

--- client/src/pages/itemDetail/ItemDescription.jsx
+++ client/src/pages/itemDetail/ItemDescription.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import mainBurger from "../../assets/images/mainBurger.png";
 import axios from "axios";
 
 function ItemDescription() {
